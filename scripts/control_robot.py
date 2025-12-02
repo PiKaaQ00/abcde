1abb4a4860fdd1097b814066bb6e088f86533c29
PiKaaQ00 <805198603@qq.com>
2025-12-02 16:17:26 +0800

    Add files via upload

---
+++ scripts/control_robot.py
@@ -0,0 +1,437 @@
+# 版权所有2024 The Huggingface Inc.团队。版权所有。
+#
+# 根据Apache许可证（版本2.0（“许可”）获得许可；
+# 除了符合许可外，您不得使用此文件。
+# 您可以在
+#
+#     http://www.apache.org/licenses/license-2.0
+#
+# 除非适用法律要求或书面同意，否则软件
+# 根据许可证分配的是按照“原样”分发
+# 没有任何明示或暗示的保证或条件。
+# 请参阅许可证的许可，以获取特定的管理权限和
+# 许可下的限制。
+"""
+Utilities to control a robot.
+
+Useful to record a dataset, replay a recorded episode, run the policy on your robot
+and record an evaluation dataset, and to recalibrate your robot if needed.
+
+Examples of usage:
+
+- Recalibrate your robot:
+```bash
+python lerobot/scripts/control_robot.py \
+    --robot.type=so100 \
+    --control.type=calibrate
+```
+
+- Unlimited teleoperation at highest frequency (~200 Hz is expected), to exit with CTRL+C:
+```bash
+python lerobot/scripts/control_robot.py \
+    --robot.type=so100 \
+    --robot.cameras='{}' \
+    --control.type=teleoperate
+
+# Add the cameras from the robot definition to visualize them:
+python lerobot/scripts/control_robot.py \
+    --robot.type=so100 \
+    --control.type=teleoperate
+```
+
+- Unlimited teleoperation at a limited frequency of 30 Hz, to simulate data recording frequency:
+```bash
+python lerobot/scripts/control_robot.py \
+    --robot.type=so100 \
+    --control.type=teleoperate \
+    --control.fps=30
+```
+
+- Record one episode in order to test replay:
+```bash
+python lerobot/scripts/control_robot.py \
+    --robot.type=so100 \
+    --control.type=record \
+    --control.fps=30 \
+    --control.single_task="Grasp a lego block and put it in the bin." \
+    --control.repo_id=$USER/koch_test \
+    --control.num_episodes=1 \
+    --control.push_to_hub=True
+```
+
+- Visualize dataset:
+```bash
+python lerobot/scripts/visualize_dataset.py \
+    --repo-id $USER/koch_test \
+    --episode-index 0
+```
+
+- Replay this test episode:
+```bash
+python lerobot/scripts/control_robot.py replay \
+    --robot.type=so100 \
+    --control.type=replay \
+    --control.fps=30 \
+    --control.repo_id=$USER/koch_test \
+    --control.episode=0
+```
+
+- Record a full dataset in order to train a policy, with 2 seconds of warmup,
+30 seconds of recording for each episode, and 10 seconds to reset the environment in between episodes:
+```bash
+python lerobot/scripts/control_robot.py record \
+    --robot.type=so100 \
+    --control.type=record \
+    --control.fps 30 \
+    --control.repo_id=$USER/koch_pick_place_lego \
+    --control.num_episodes=50 \
+    --control.warmup_time_s=2 \
+    --control.episode_time_s=30 \
+    --control.reset_time_s=10
+```
+
+- For remote controlled robots like LeKiwi, run this script on the robot edge device (e.g. RaspBerryPi):
+```bash
+python lerobot/scripts/control_robot.py \
+  --robot.type=lekiwi \
+  --control.type=remote_robot
+```
+
+**NOTE**: You can use your keyboard to control data recording flow.
+- Tap right arrow key '->' to early exit while recording an episode and go to resseting the environment.
+- Tap right arrow key '->' to early exit while resetting the environment and got to recording the next episode.
+- Tap left arrow key '<-' to early exit and re-record the current episode.
+- Tap escape key 'esc' to stop the data recording.
+This might require a sudo permission to allow your terminal to monitor keyboard events.
+
+**NOTE**: You can resume/continue data recording by running the same data recording command and adding `--control.resume=true`.
+
+- Train on this dataset with the ACT policy:
+```bash
+python lerobot/scripts/train.py \
+  --dataset.repo_id=${HF_USER}/koch_pick_place_lego \
+  --policy.type=act \
+  --output_dir=outputs/train/act_koch_pick_place_lego \
+  --job_name=act_koch_pick_place_lego \
+  --device=cuda \
+  --wandb.enable=true
+```
+
+- Run the pretrained policy on the robot:
+```bash
+python lerobot/scripts/control_robot.py \
+    --robot.type=so100 \
+    --control.type=record \
+    --control.fps=30 \
+    --control.single_task="Grasp a lego block and put it in the bin." \
+    --control.repo_id=$USER/eval_act_koch_pick_place_lego \
+    --control.num_episodes=10 \
+    --control.warmup_time_s=2 \
+    --control.episode_time_s=30 \
+    --control.reset_time_s=10 \
+    --control.push_to_hub=true \
+    --control.policy.path=outputs/train/act_koch_pick_place_lego/checkpoints/080000/pretrained_model
+```
+"""
+
+import logging
+import os
+import time
+from dataclasses import asdict
+from pprint import pformat
+
+import rerun as rr
+
+# 从 safetensors.torch 导入 load_file, save_file
+from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
+from lerobot.common.policies.factory import make_policy
+from lerobot.common.robot_devices.control_configs import (
+    CalibrateControlConfig,
+    ControlConfig,
+    ControlPipelineConfig,
+    RecordControlConfig,
+    RemoteRobotConfig,
+    ReplayControlConfig,
+    TeleoperateControlConfig,
+)
+from lerobot.common.robot_devices.control_utils import (
+    control_loop,
+    init_keyboard_listener,
+    is_headless,
+    log_control_info,
+    record_episode,
+    reset_environment,
+    sanity_check_dataset_name,
+    sanity_check_dataset_robot_compatibility,
+    stop_recording,
+    warmup_record,
+)
+from lerobot.common.robot_devices.robots.utils import Robot, make_robot_from_config
+from lerobot.common.robot_devices.utils import busy_wait, safe_disconnect
+from lerobot.common.utils.utils import has_method, init_logging, log_say
+from lerobot.configs import parser
+
+########################################################################################
+# 控制模式
+########################################################################################
+
+
+@safe_disconnect
+def calibrate(robot: Robot, cfg: CalibrateControlConfig):
+    # TODO(aliberts)：将此代码移动到机器人的类中
+    if robot.robot_type.startswith("stretch"):
+        if not robot.is_connected:
+            robot.connect()
+        if not robot.is_homed():
+            robot.home()
+        return
+
+    arms = robot.available_arms if cfg.arms is None else cfg.arms
+    unknown_arms = [arm_id for arm_id in arms if arm_id not in robot.available_arms]
+    available_arms_str = " ".join(robot.available_arms)
+    unknown_arms_str = " ".join(unknown_arms)
+
+    if arms is None or len(arms) == 0:
+        raise ValueError(
+            "No arm provided. Use `--arms` as argument with one or more available arms.\n"
+            f"For instance, to recalibrate all arms add: `--arms {available_arms_str}`"
+        )
+
+    if len(unknown_arms) > 0:
+        raise ValueError(
+            f"Unknown arms provided ('{unknown_arms_str}'). Available arms are `{available_arms_str}`."
+        )
+
+    for arm_id in arms:
+        arm_calib_path = robot.calibration_dir / f"{arm_id}.json"
+        if arm_calib_path.exists():
+            print(f"Removing '{arm_calib_path}'")
+            arm_calib_path.unlink()
+        else:
+            print(f"Calibration file not found '{arm_calib_path}'")
+
+    if robot.is_connected:
+        robot.disconnect()
+
+    if robot.robot_type.startswith("lekiwi") and "main_follower" in arms:
+        print("Calibrating only the lekiwi follower arm 'main_follower'...")
+        robot.calibrate_follower()
+        return
+
+    if robot.robot_type.startswith("lekiwi") and "main_leader" in arms:
+        print("Calibrating only the lekiwi leader arm 'main_leader'...")
+        robot.calibrate_leader()
+        return
+
+    # 调用“连接”自动运行校准
+    # 当缺少校准文件时
+    robot.connect()
+    robot.disconnect()
+    print("Calibration is done! You can now teleoperate and record datasets!")
+
+
+@safe_disconnect
+def teleoperate(robot: Robot, cfg: TeleoperateControlConfig):
+    control_loop(
+        robot,
+        control_time_s=cfg.teleop_time_s,
+        fps=cfg.fps,
+        teleoperate=True,
+        display_data=cfg.display_data,
+    )
+
+
+@safe_disconnect
+def record(
+    robot: Robot,
+    cfg: RecordControlConfig,
+) -> LeRobotDataset:
+    # TODO（RCADENE）：添加选项以记录日志
+    if cfg.resume:
+        dataset = LeRobotDataset(
+            cfg.repo_id,
+            root=cfg.root,
+        )
+        if len(robot.cameras) > 0:
+            dataset.start_image_writer(
+                num_processes=cfg.num_image_writer_processes,
+                num_threads=cfg.num_image_writer_threads_per_camera * len(robot.cameras),
+            )
+        sanity_check_dataset_robot_compatibility(dataset, robot, cfg.fps, cfg.video)
+    else:
+        # 创建空数据集或加载现有已保存的剧集
+        sanity_check_dataset_name(cfg.repo_id, cfg.policy)
+        dataset = LeRobotDataset.create(
+            cfg.repo_id,
+            cfg.fps,
+            root=cfg.root,
+            robot=robot,
+            use_videos=cfg.video,
+            image_writer_processes=cfg.num_image_writer_processes,
+            image_writer_threads=cfg.num_image_writer_threads_per_camera * len(robot.cameras),
+        )
+
+    # 加载预训练策略
+    policy = None if cfg.policy is None else make_policy(cfg.policy, ds_meta=dataset.meta)
+
+    if not robot.is_connected:
+        robot.connect()
+
+    listener, events = init_keyboard_listener()
+
+    # 执行几秒钟而不记录到：
+    # 1. 如果没有提供策略，则远程操作机器人将其移至起始位置，
+    # 2. 给机器人设备连接并开始同步的时间，
+    # 3. 将相机窗口放置在屏幕上
+    enable_teleoperation = policy is None
+    log_say("Warmup record", cfg.play_sounds)
+    warmup_record(robot, events, enable_teleoperation, cfg.warmup_time_s, cfg.display_data, cfg.fps)
+
+    if has_method(robot, "teleop_safety_stop"):
+        robot.teleop_safety_stop()
+
+    recorded_episodes = 0
+    while True:
+        if recorded_episodes >= cfg.num_episodes:
+            break
+
+        log_say(f"Recording episode {dataset.num_episodes}", cfg.play_sounds)
+        record_episode(
+            robot=robot,
+            dataset=dataset,
+            events=events,
+            episode_time_s=cfg.episode_time_s,
+            display_data=cfg.display_data,
+            policy=policy,
+            fps=cfg.fps,
+            single_task=cfg.single_task,
+        )
+
+        # 执行几秒钟而不记录，以便有时间手动重置环境
+        # 当前的代码逻辑不允许在此期间进行远程操作。
+        # TODO(rcadene)：添加一个选项以在重置期间启用远程操作
+        # 跳过重置以记录最后一集
+        if not events["stop_recording"] and (
+            (recorded_episodes < cfg.num_episodes - 1) or events["rerecord_episode"]
+        ):
+            log_say("Reset the environment", cfg.play_sounds)
+            reset_environment(robot, events, cfg.reset_time_s, cfg.fps)
+
+        if events["rerecord_episode"]:
+            log_say("Re-record episode", cfg.play_sounds)
+            events["rerecord_episode"] = False
+            events["exit_early"] = False
+            dataset.clear_episode_buffer()
+            continue
+
+        dataset.save_episode()
+        recorded_episodes += 1
+
+        if events["stop_recording"]:
+            break
+
+    log_say("Stop recording", cfg.play_sounds, blocking=True)
+    stop_recording(robot, listener, cfg.display_data)
+
+    if cfg.push_to_hub:
+        dataset.push_to_hub(tags=cfg.tags, private=cfg.private)
+
+    log_say("Exiting", cfg.play_sounds)
+    return dataset
+
+
+@safe_disconnect
+def replay(
+    robot: Robot,
+    cfg: ReplayControlConfig,
+):
+    # TODO（RCADENE，ALIBERTS）：带有Control_loop的重构，一旦`dataSet'是lerobotdataset的一个实例
+    # TODO（RCADENE）：添加选项以记录日志
+
+    dataset = LeRobotDataset(cfg.repo_id, root=cfg.root, episodes=[cfg.episode])
+    actions = dataset.hf_dataset.select_columns("action")
+
+    if not robot.is_connected:
+        robot.connect()
+
+    log_say("Replaying episode", cfg.play_sounds, blocking=True)
+    for idx in range(dataset.num_frames):
+        start_episode_t = time.perf_counter()
+
+        action = actions[idx]["action"]
+        robot.send_action(action)
+
+        dt_s = time.perf_counter() - start_episode_t
+        busy_wait(1 / cfg.fps - dt_s)
+
+        dt_s = time.perf_counter() - start_episode_t
+        log_control_info(robot, dt_s, fps=cfg.fps)
+
+
+def _init_rerun(control_config: ControlConfig, session_name: str = "lerobot_control_loop") -> None:
+    """Initializes the Rerun SDK for visualizing the control loop.
+
+    Args:
+        control_config: Configuration determining data display and robot type.
+        session_name: Rerun session name. Defaults to "lerobot_control_loop".
+
+    Raises:
+        ValueError: If viewer IP is missing for non-remote configurations with display enabled.
+    """
+    if (control_config.display_data and not is_headless()) or (
+        control_config.display_data and isinstance(control_config, RemoteRobotConfig)
+    ):
+        # 如果未设置，则配置重新运行刷新批量大小默认为 8KB
+        batch_size = os.getenv("RERUN_FLUSH_NUM_BYTES", "8000")
+        os.environ["RERUN_FLUSH_NUM_BYTES"] = batch_size
+
+        # 根据配置初始化重新运行
+        rr.init(session_name)
+        if isinstance(control_config, RemoteRobotConfig):
+            viewer_ip = control_config.viewer_ip
+            viewer_port = control_config.viewer_port
+            if not viewer_ip or not viewer_port:
+                raise ValueError(
+                    "Viewer IP & Port are required for remote config. Set via config file/CLI or disable control_config.display_data."
+                )
+            logging.info(f"Connecting to viewer at {viewer_ip}:{viewer_port}")
+            rr.connect_tcp(f"{viewer_ip}:{viewer_port}")
+        else:
+            # 获取重新运行查看器参数的内存限制
+            memory_limit = os.getenv("LEROBOT_RERUN_MEMORY_LIMIT", "10%")
+            rr.spawn(memory_limit=memory_limit)
+
+
+@parser.wrap()
+def control_robot(cfg: ControlPipelineConfig):
+    init_logging()
+    logging.info(pformat(asdict(cfg)))
+
+    robot = make_robot_from_config(cfg.robot)
+
+    # TODO(Steven)：固定窗口大小的蓝图
+
+    if isinstance(cfg.control, CalibrateControlConfig):
+        calibrate(robot, cfg.control)
+    elif isinstance(cfg.control, TeleoperateControlConfig):
+        _init_rerun(control_config=cfg.control, session_name="lerobot_control_loop_teleop")
+        teleoperate(robot, cfg.control)
+    elif isinstance(cfg.control, RecordControlConfig):
+        _init_rerun(control_config=cfg.control, session_name="lerobot_control_loop_record")
+        record(robot, cfg.control)
+    elif isinstance(cfg.control, ReplayControlConfig):
+        replay(robot, cfg.control)
+    elif isinstance(cfg.control, RemoteRobotConfig):
+        from lerobot.common.robot_devices.robots.lekiwi_remote import run_lekiwi
+
+        _init_rerun(control_config=cfg.control, session_name="lerobot_control_loop_remote")
+        run_lekiwi(cfg.robot)
+
+    if robot.is_connected:
+        # 手动断开连接，以避免在过程中避免“核心转储”
+        # 由于摄像头螺纹导致的终止未正确退出。
+        robot.disconnect()
+
+
+if __name__ == "__main__":
+    control_robot()
